2cdb07abbf98085b7014c527f9fc99860b6b04d0
Dzmitry Kotuseu <dzmitry.kotuseu@murka.com>
2021-11-06 01:06:26 +0100

    Some progress.

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,9 +1,13 @@
-using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
@@ -17,4 +21,13 @@ public class RTSNetworkManager : NetworkManager
         NetworkServer.Spawn(unitSpawnerInstance, conn);
     }
 
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        if(SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
+    }
 }
